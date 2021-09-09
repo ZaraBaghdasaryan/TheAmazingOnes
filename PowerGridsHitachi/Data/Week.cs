64ecac21498b6d356a77221f44586bcb0f21955a
Zara <zara.baghdasaryan@yahoo.com>
2021-09-09 09:18:43 +0200

    Blazor Calender

---
+++ PowerGridsHitachi/Data/Week.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ namespace UI.Data
+{
+    public class Week
+    {
+        public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+    }
+}
--- PowerGridsHitachi/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Counter.razor.g.cs
+++ PowerGridsHitachi/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,64 +13,57 @@ namespace UI.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 1 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 2 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 3 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 4 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 5 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 6 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 7 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
+#line 8 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
 using PowerGridsHitachi;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 9 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\_Imports.razor"
-using PowerGridsHitachi.Shared;
 
 #line default
 #line hidden
@@ -84,7 +77,7 @@ using PowerGridsHitachi.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\Admin\source\repos\PowerGridsHitachi\PowerGridsHitachi\Pages\Counter.razor"
+#line 9 "C:\Users\Admin\OneDrive\Desktop\LIA\PowerGridsHitachi\PowerGridsHitachi\Pages\Counter.razor"
        
     private int currentCount = 0;
 
